8a376ff73f5dff6406abe3346f94256983a215a6
Graves067 <c.a.gillespie9@gmail.com>
2025-04-11 00:13:58 -0700

    removed unneeded item

--- PHP/Config.php
+++ PHP/Config.php
@@ -4,7 +4,7 @@
 // Replace these values with your own database connection settings
 $servername = "localhost";  // or your server
 $username = "root";         // database username
-$password = "Compass1234!";             // database password
+$password = "";             // database password
 $dbname = "sockazon";       // your database name
 
 try {
